9fd0703ad8fe384a46eb40e19ae42c79300cb187
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-12-26 17:36:34 +0900

    bubble sort

---
+++ algorithm/src/BubbleSort/BubbleSort.java
@@ -0,0 +1,26 @@
+package BubbleSort;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+
+		int[] data = {4, 5, 1, 3, 2, 6};
+
+		for (int i = 0; i + 1 < data.length; i++) {
+
+			for (int j = 0; j + 1 < data.length - i; j++) {
+
+				if (data[j] > data[j + 1]) {
+					int tmp = data[j];
+					data[j] = data[j + 1];
+					data[j + 1] = tmp;
+				}
+				for (int n = 0; n < data.length; n++) {
+					System.out.print(data[n]);
+				}
+				System.out.println("");
+			}
+			System.out.println("----------");
+		}
+	}
+}
